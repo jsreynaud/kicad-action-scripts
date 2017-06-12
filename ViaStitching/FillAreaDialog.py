a88918ecf2c27f11fe919a5e06e4acb5c0706322
Cristi Marius Tiutiu <mtiutiu@gmail.com>
2017-06-12 17:25:42 +0300

    better description for delete vias checkbox

--- ViaStitching/FillAreaDialog.py
+++ ViaStitching/FillAreaDialog.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 
 ###########################################################################
-## Python code generated with wxFormBuilder (version Jun 12 2017)
+## Python code generated with wxFormBuilder (version Feb 16 2016)
 ## http://www.wxformbuilder.org/
 ##
 ## PLEASE DO "NOT" EDIT THIS FILE!
@@ -15,102 +15,104 @@ import wx.xrc
 ###########################################################################
 
 class FillAreaDialog ( wx.Dialog ):
-
+	
 	def __init__( self, parent ):
 		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Fill Area parameters", pos = wx.DefaultPosition, size = wx.Size( 369,389 ), style = wx.DEFAULT_DIALOG_STYLE )
-
+		
 		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
-
+		
 		fgSizer1 = wx.FlexGridSizer( 0, 2, 0, 0 )
 		fgSizer1.SetFlexibleDirection( wx.BOTH )
 		fgSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
-
+		
 		self.m_staticText3 = wx.StaticText( self, wx.ID_ANY, u"Via copper size", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText3.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText3, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_SizeMM = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_SizeMM.SetMinSize( wx.Size( 1000,-1 ) )
-
+		
 		fgSizer1.Add( self.m_SizeMM, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_staticText9 = wx.StaticText( self, wx.ID_ANY, u"Via drill size", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText9.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText9, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_DrillMM = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		fgSizer1.Add( self.m_DrillMM, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_staticText5 = wx.StaticText( self, wx.ID_ANY, u"Via clearance", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText5.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText5, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_ClearanceMM = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		fgSizer1.Add( self.m_ClearanceMM, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_staticText6 = wx.StaticText( self, wx.ID_ANY, u"Net name", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText6.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText6, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_Netname = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		fgSizer1.Add( self.m_Netname, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Step between via", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText2.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText2, 0, wx.ALL, 5 )
-
+		
 		self.m_StepMM = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		fgSizer1.Add( self.m_StepMM, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_staticText7 = wx.StaticText( self, wx.ID_ANY, u"Debug mode", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText7.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText7, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_Debug = wx.CheckBox( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		fgSizer1.Add( self.m_Debug, 1, wx.ALL|wx.EXPAND, 5 )
-
+		
 		self.m_staticText8 = wx.StaticText( self, wx.ID_ANY, u"Random it", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText8.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText8, 0, wx.ALL, 5 )
-
+		
 		self.m_Random = wx.CheckBox( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		fgSizer1.Add( self.m_Random, 0, wx.ALL, 5 )
-
+		
 		self.m_staticText81 = wx.StaticText( self, wx.ID_ANY, u"Only under selected Zone", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText81.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText81, 0, wx.ALL, 5 )
-
+		
 		self.m_only_selected = wx.CheckBox( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		fgSizer1.Add( self.m_only_selected, 0, wx.ALL, 5 )
-
-		self.m_staticText10 = wx.StaticText( self, wx.ID_ANY, u"Delete vias", wx.DefaultPosition, wx.DefaultSize, 0 )
+		
+		self.m_staticText10 = wx.StaticText( self, wx.ID_ANY, u"Delete generated vias", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText10.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText10, 0, wx.ALL, 5 )
-
+		
 		self.m_delete_vias = wx.CheckBox( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		fgSizer1.Add( self.m_delete_vias, 0, wx.ALL, 5 )
-
+		
 		self.m_staticText71 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText71.Wrap( -1 )
 		fgSizer1.Add( self.m_staticText71, 0, wx.ALL, 5 )
-
+		
 		bSizer1 = wx.BoxSizer( wx.HORIZONTAL )
-
+		
 		self.m_button1 = wx.Button( self, wx.ID_OK, u"Run", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_button1.SetDefault()
+		self.m_button1.SetDefault() 
 		bSizer1.Add( self.m_button1, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
-
+		
 		self.m_button2 = wx.Button( self, wx.ID_CANCEL, u"Cancel", wx.DefaultPosition, wx.DefaultSize, 0 )
 		bSizer1.Add( self.m_button2, 0, wx.ALL, 5 )
-
-
+		
+		
 		fgSizer1.Add( bSizer1, 1, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
-
-
+		
+		
 		self.SetSizer( fgSizer1 )
 		self.Layout()
-
+		
 		self.Centre( wx.BOTH )
-
+	
 	def __del__( self ):
 		pass
+	
+
